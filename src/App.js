24de99fbe714284f3f68a665ce360c8388a188ac
nathanlm511 <nathanmoeliono@gmail.com>
2020-11-18 11:48:44 -0500

    added basic ui foundation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './Filter.scss';
+import React, { useState, Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 28
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="column left-column">
+          <div className="presets card">
+            <p>Basic Presets</p>
+            <div className="preset-container">
+              <div className="preset-row">
+                <div className="preset">
+                  <button>click</button>
+                </div>
+                <div className="preset">
+                  <button>click</button>
+                </div>
+              </div>
+              <div className="preset-row">
+                <div className="preset">
+                  <button>click</button>
+                </div>
+                <div className="preset">
+                  <button>click</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="presets card">
+            <p>Other Presets</p>
+            <div className="preset-container">
+              <div className="preset-row">
+                <div className="preset">
+                  <button>click</button>
+                </div>
+                <div className="preset">
+                  <button>click</button>
+                </div>
+              </div>
+              <div className="preset-row">
+                <div className="preset">
+                  <button>click</button>
+                </div>
+                <div className="preset">
+                  <button>click</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column center-column">
+          <div className="title">
+            CSS Generator
+          </div>
+          <div className="button-window card">
+            <button>
+              Click me
+            </button>
+          </div>
+          <div className="json-window card">
+            This is text\nwith a stdfdf\nsdfsdfsdf\nsdfdsfsdfsd\n
+          </div>
+        </div>
+        <div className="column right-column">
+          <div className="filters card">
+            <p>Color filters</p>
+            <div className="filters-container">
+              <div className="range-slider">
+                <input className="range-slider__range" type="range" value={this.state.test} onChange={(e) => this.setState({test: e.target.value})} min="0" max="100"/>
+                <span className="range-slider__value">0</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
-
-export default App;
